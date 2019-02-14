9b1e018d9c3e3a83274dc5df8921f28d3ad8ae30
zacanger <zac@zacanger.com>
2019-02-13 21:49:10 -0700

    run standard

--- test/index.js
+++ test/index.js
@@ -3,13 +3,14 @@
  */
 
 var koa = require('koa')
-  , should = require('should');
 
-describe('module', function() {
-  it('should expose Router', function(done) {
-    var Router = require('..');
-    should.exist(Router);
-    Router.should.be.type('function');
-    done();
-  });
-});
+var should = require('should')
+
+describe('module', function () {
+  it('should expose Router', function (done) {
+    var Router = require('..')
+    should.exist(Router)
+    Router.should.be.type('function')
+    done()
+  })
+})
